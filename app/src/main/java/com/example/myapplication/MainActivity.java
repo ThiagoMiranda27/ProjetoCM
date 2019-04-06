ff898b18ab4050a952160591e4c2fd687953dcf3
unknown <thi_cmiranda@hotmail.comgit config --global user.name ThiagoMiranda27>
2019-04-06 13:52:14 -0300

    aula4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,84 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    private int NOTIFICATION_PERMISSION_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton banner = findViewById(R.id.banner);
+        banner.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                if(ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this, "Voce ja concedeu esta permissao!", Toast.LENGTH_SHORT).show();
+                }else{
+                    requestStoragePermission();
+                }
+            }
+        });
     }
+
+    private void requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+            new AlertDialog.Builder(this).setTitle("Permissao necessaria")
+                    .setMessage("Essa permissao é necessaria para receber as notificaçoes")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, NOTIFICATION_PERMISSION_CODE);
+
+                }
+            })
+                    .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        }else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, NOTIFICATION_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == NOTIFICATION_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permissao concedida", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permissao negada", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
 }
+
+
