e695c01ae051837ebeef232071cdc091353d6a12
unknown <thi_cmiranda@hotmail.comgit config --global user.name ThiagoMiranda27>
2019-04-16 19:00:33 -0300

    aula5

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,14 +11,23 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private int NOTIFICATION_PERMISSION_CODE = 1;
 
+    int[] IMAGES = {R.drawable.gabinete, R.drawable.memoria, R.drawable.placa_de_video, R.drawable.processador};
 
+    String[] NAMES = {"Gabinete AERO", "Memoria DDR4 HypesX", "Placa de video 1060 ti", "Processador I7"};
+
+    String[] DESCRIPTIONS ={"Gabinete AERO R$250,00", "MERORIA DDR4 HyperX 2400hz R$500,00", "Placa de video 1060 ti zotac R$1200,00", "Processador I7 8700 R$1600,00" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,43 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        ListView listView = findViewById(R.id.listView);
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+    }
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return IMAGES.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.customlayout, null);
+
+            ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
+            TextView textView_name = (TextView)view.findViewById(R.id.textView_name);
+            TextView textView_description = (TextView)view.findViewById(R.id.textView_description);
+
+            imageView.setImageResource(IMAGES[position]);
+            textView_name.setText(NAMES[position]);
+            textView_description.setText(DESCRIPTIONS[position]);
+
+            return view;
+        }
     }
 
     private void requestStoragePermission(){
