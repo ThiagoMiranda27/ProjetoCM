0ee26a4bbb5dbca6aa3d6d3fc9f782669f347ae4
unknown <thi_cmiranda@hotmail.comgit config --global user.name ThiagoMiranda27>
2019-06-11 21:41:08 -0300

    style and others

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,14 +1,21 @@
 package com.example.myapplication;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.databinding.DataBindingUtil;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
 import com.example.myapplication.User;
 
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity<fun> extends AppCompatActivity {
     private static final String TAG = "Main2Activity";
+    private Intent motionEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,24 @@ public class Main2Activity extends AppCompatActivity {
         binding.User(user);
 
         Log.i(TAG,"Tela de Login");
+
+        findViewById(R.id.imageViewDrag).setOnTouchListener(MyTouchListener());
+
     }
+
+//    private View.OnTouchListener MyTouchListener() {
+//
+//        fun onTouch(view:View, motionEvent:MotionEvent):Boolean {
+//            if (motionEvent.getAction() === MotionEvent.ACTION_DOWN){
+//                val data = ClipData.newPlainText("", "");
+//                val shadowBuilder = View.DragShadowBuilder(
+//                        view);
+//                view.startDrag(data, shadowBuilder, view, 0);
+//                view.setVisibility(View.INVISIBLE);
+//                return true
+//            } else {
+//                return false
+//            }
+//        }
+//    }
 }
